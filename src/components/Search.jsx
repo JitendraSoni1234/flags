de896a16c8724baf4d7d42836575536cd4e671d0
Jitendra soni <84441277+JitendraSoni1234@users.noreply.github.com>
2023-04-08 13:30:55 +0530

    code fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,6 +4,7 @@ import { Form } from "react-router-dom";
 
 function Search({ text, setText, setValue }) {
   const handleChange = event => {
+    event.preventDefault();
     setText(event.target.value);
     setValue({ search: event.target.value });
   };
@@ -11,9 +12,9 @@ function Search({ text, setText, setValue }) {
   return (
     <div className="w-full flex items-center gap-8 px-10 py-5 text-color bg-color-component shadow rounded-md sm:w-[400px] sm:gap-6 md:px-8 ease-in-out duration-300">
       <MagnifyingGlass className="w-[25px] h-[25px]" />
-      <Form name="search" autoComplete="off">
+      {/* <Form name="search" autoComplete="off"> */}
         <input className="w-full bg-transparent focus:outline-none" placeholder="Search for a country..." value={text} onChange={handleChange} />
-      </Form>
+      {/* </Form> */}
     </div>
   );
 }
